f01ba81b2e0c42247e951f96d4e2490a3899fcd0
BotEthan <ethanclack1@gmail.com>
2022-07-13 10:10:07 +0200

    Added comments and modulo

--- test.py
+++ test.py
@@ -1,7 +1,9 @@
 def Start():
+    #Collect the numbers and signs
     numberOne = input("What is the first number? ")
     sign = input("What is the sign? ")
     numberTwo = input("What is the second number? ")
+    #Figure out what sign is used
     match sign:
         case "+":
             print(f'{numberOne} + {numberTwo} = {int(numberOne) + int(numberTwo)}')
@@ -9,8 +11,10 @@ def Start():
             print(f'{numberOne} - {numberTwo} = {int(numberOne) - int(numberTwo)}')
         case "*":
             print(f'{numberOne} * {numberTwo} = {int(numberOne) * int(numberTwo)}')
-        case "/":
+        case "/":                                                                       #Print calculation
             print(f'{numberOne} / {numberTwo} = {int(numberOne) / int(numberTwo)}')
+        case "%":
+            print(f'{numberOne} % {numberTwo} = {int(numberOne) % int(numberTwo)}')
 
 if __name__ == "__main__":
     Start()
